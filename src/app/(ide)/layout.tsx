6e01c875daf78279a0c0b84282d51e27e2d9a509
JM Mac <johnmarkmancol@gmail.com>
2025-01-21 18:46:48 +0800

    feat: connect wallet feature using Web3-Onboard

--- src/app/(ide)/layout.tsx
+++ src/app/(ide)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en-US">
       <body>
         <SidebarIcons />
         <SidebarProvider className="w-full">
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import ConnectWallet from "./connect-wallet";
 
 export function Header() {
   return (
     <div className="flex items-center gap-4 justify-between border-b p-2">
       <Input placeholder="Type a command or search..." className="w-80" />
-      <Button>Connect to Metamask</Button>
+      <ConnectWallet />
     </div>
   );
 }
