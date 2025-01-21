5f23d2ef9b073236e7113c001a688209bb174526
JM Mac <johnmarkmancol@gmail.com>
2025-01-21 10:57:41 +0800

    style: added background to side navigations

--- src/components/sidebar-icons.tsx
+++ src/components/sidebar-icons.tsx
@@ -12,14 +12,14 @@ export function SidebarIcons() {
   const pathname = usePathname();
 
   return (
-    <div className="fixed top-0 h-full left-0 w-12 z-20 border-r flex flex-col">
+    <div className="fixed top-0 h-full left-0 w-12 z-20 border-r flex flex-col bg-muted">
       <ul className="flex flex-col items-center">
         {SideBarIconMenu.map((item, nav_key) => (
           <li key={`icon-nav-${nav_key}`} className="w-full">
             <Link
               href={item.url}
               className={`flex items-center justify-center p-2 w-full relative ${
-                pathname == item.url && "bg-muted"
+                pathname == item.url && "bg-accent"
               }`}
             >
               {pathname == item.url && (
