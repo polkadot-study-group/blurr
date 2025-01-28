6e38b127f2d8faf26d67df96676d572440e735ef
JM Mac <johnmarkmancol@gmail.com>
2025-01-28 13:56:55 +0800

    fix: code editor theme

--- src/components/tabs/code-editor.tsx
+++ src/components/tabs/code-editor.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { RootState } from "@/store/store";
-import Editor from "@monaco-editor/react";
+import Editor, { useMonaco } from "@monaco-editor/react";
 import { useSelector } from "react-redux";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
@@ -10,26 +10,29 @@ export function CodeEditor() {
   const tabs = useSelector((state: RootState) => state.workspaceTabs);
   const { theme, systemTheme } = useTheme();
   const [currTheme, setCurrTheme] = useState("vs-light");
+  const monaco = useMonaco();
 
   useEffect(() => {
     let selectedTheme = "vs-light";
     if (theme === "system") {
-      selectedTheme = systemTheme === "dark" ? "vs-dark" : "vs-light";
+      if (systemTheme === "dark") {
+        selectedTheme = "vs-dark";
+      }
     } else if (theme === "dark") {
       selectedTheme = "vs-dark";
     }
     setCurrTheme(selectedTheme);
+    monaco?.editor.setTheme(selectedTheme);
   }, [theme, systemTheme]);
 
   return (
-    <Editor
-      height="100%"
-      defaultLanguage="html"
-      value={tabs.active.content}
-      theme={currTheme}
-      onMount={(editor, monaco) => {
-        monaco.editor.setTheme(currTheme); // Apply theme on mount
-      }}
-    />
+    <>
+      <Editor
+        height="100%"
+        defaultLanguage="html"
+        value={tabs.active.content}
+        theme={currTheme}
+      />
+    </>
   );
 }
