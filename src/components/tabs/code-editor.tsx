647d22f0d87c8543363e7974ff366c8c684df6c1
JM Mac <johnmarkmancol@gmail.com>
2025-01-28 13:12:54 +0800

    feat: apply theme color on code editor

--- src/components/tabs/code-editor.tsx
+++ src/components/tabs/code-editor.tsx
@@ -1,18 +1,35 @@
 "use client";
+
 import { RootState } from "@/store/store";
 import Editor from "@monaco-editor/react";
 import { useSelector } from "react-redux";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function CodeEditor() {
   const tabs = useSelector((state: RootState) => state.workspaceTabs);
+  const { theme, systemTheme } = useTheme();
+  const [currTheme, setCurrTheme] = useState("vs-light");
+
+  useEffect(() => {
+    let selectedTheme = "vs-light";
+    if (theme === "system") {
+      selectedTheme = systemTheme === "dark" ? "vs-dark" : "vs-light";
+    } else if (theme === "dark") {
+      selectedTheme = "vs-dark";
+    }
+    setCurrTheme(selectedTheme);
+  }, [theme, systemTheme]);
 
   return (
-    <>
-      <Editor
-        height="100%"
-        defaultLanguage="html"
-        value={tabs.active.content}
-      />
-    </>
+    <Editor
+      height="100%"
+      defaultLanguage="html"
+      value={tabs.active.content}
+      theme={currTheme}
+      onMount={(editor, monaco) => {
+        monaco.editor.setTheme(currTheme); // Apply theme on mount
+      }}
+    />
   );
 }
