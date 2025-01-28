df5ade54e2f5781b0e5cedd8d01629ecebe8d13b
JM Mac <johnmarkmancol@gmail.com>
2025-01-28 11:16:12 +0800

    feat: setup shadcn theme provider

--- src/app/(ide)/layout.tsx
+++ src/app/(ide)/layout.tsx
@@ -6,20 +6,28 @@ import { Toaster } from "@/components/ui/toaster";
 import { MainSidebar } from "@/components/sidebar/main-sidebar";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function RootLayout() {
   return (
     <html lang="en-US">
       <body>
-        <Provider store={store}>
-          <div className="flex h-screen w-screen">
-            <MainSidebar />
-            <main className="flex-1 pl-80">
-              <Workspace />
-            </main>
-          </div>
-          <Toaster />
-        </Provider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Provider store={store}>
+            <div className="flex h-screen w-screen">
+              <MainSidebar />
+              <main className="flex-1 pl-80">
+                <Workspace />
+              </main>
+            </div>
+            <Toaster />
+          </Provider>
+        </ThemeProvider>
       </body>
     </html>
   );
