20bc420517b27ebb075d41c57aa8ece5b1020a18
JM Mac <johnmarkmancol@gmail.com>
2025-02-14 10:31:53 +0800

    feat: added action buttons on Folder Tree

--- src/components/sidebar/sidebar-contents/file-explorer.tsx
+++ src/components/sidebar/sidebar-contents/file-explorer.tsx
@@ -12,6 +12,18 @@ import "react-folder-tree/dist/style.css";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import { getFilesRecursive } from "@/services/file.service";
+import { SampleFileTreeData } from "@/defaults/sample-files.data";
+import {
+  Delete,
+  Edit,
+  File,
+  Folder,
+  FolderOpen,
+  Home,
+  PlusIcon,
+  TrashIcon,
+  X,
+} from "lucide-react";
 
 const FolderTree = dynamic(() => import("react-folder-tree"), { ssr: false });
 
@@ -43,6 +55,65 @@ export default function FileExplorer() {
     console.log(nodeData);
   };
 
+  const FolderIcon = ({ defaultOnClick, nodeData }: TreeEventProps) => {
+    const { path, name, checked, isOpen, ...restData } = nodeData;
+    const handleClick = () => {
+      defaultOnClick();
+    };
+
+    return <Folder className="size-4" onClick={handleClick} />;
+  };
+
+  const FolderOpenIcon = ({ defaultOnClick, nodeData }: TreeEventProps) => {
+    const { path, name, checked, isOpen, ...restData } = nodeData;
+    const handleClick = () => {
+      // doSthBad({ path, name, checked, isOpen, ...restData });
+      defaultOnClick();
+    };
+
+    return <FolderOpen className="size-4" onClick={handleClick} />;
+  };
+
+  const FileIcon = ({ defaultOnClick, nodeData }: TreeEventProps) => {
+    const { path, name, checked, isOpen, ...restData } = nodeData;
+    const handleClick = () => {
+      defaultOnClick();
+    };
+
+    return <File className="size-4" onClick={handleClick} />;
+  };
+
+  // const EditIcon = ({ defaultOnClick, nodeData }: TreeEventProps) => {
+  //   const { path, name, checked, isOpen, ...restData } = nodeData;
+  //   const handleClick = () => {
+  //     doSthBad({ path, name, checked, isOpen, ...restData });
+  //     defaultOnClick();
+  //     alert("edit icon clicked");
+  //   };
+
+  //   return <Edit className="size-4" onClick={handleClick} />;
+  // };
+
+  const DeleteIcon = ({ defaultOnClick, nodeData }: TreeEventProps) => {
+    const { path, name, checked, isOpen, ...restData } = nodeData;
+    const handleClick = () => {
+      alert("delete icon clicked");
+      defaultOnClick();
+    };
+
+    return <TrashIcon className="size-4" onClick={handleClick} />;
+  };
+
+  const CancelIcon = ({ defaultOnClick, nodeData }: TreeEventProps) => {
+    const { path, name, checked, isOpen, ...restData } = nodeData;
+    const handleClick = () => {
+      alert("Cancel icon clicked");
+      defaultOnClick();
+    };
+
+    return <X className="size-4" onClick={handleClick} />;
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <div className="p-2 flex flex-col gap-2">
@@ -66,8 +137,16 @@ export default function FileExplorer() {
             <FolderTree
               data={treeData}
               showCheckbox={false}
-              readOnly
               onNameClick={handleTreeNodeClick}
+              iconComponents={{
+                FolderOpenIcon,
+                FolderIcon,
+                FileIcon,
+                // CancelIcon,
+                // DeleteIcon,
+                // EditIcon
+                /* other custom icons ... */
+              }}
             />
           )}
         </div>
